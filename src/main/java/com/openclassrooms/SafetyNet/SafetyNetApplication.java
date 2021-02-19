25200cd16358c6581a779ee451ae73f2b0c93f12
matthieu <msanvee@gmail.com>
2021-02-19 18:52:49 +0100

    restok

--- src/main/java/com/openclassrooms/SafetyNet/SafetyNetApplication.java
+++ src/main/java/com/openclassrooms/SafetyNet/SafetyNetApplication.java
@@ -1,13 +1,27 @@
 package com.openclassrooms.SafetyNet;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.SafetyNet.model.Database;
 
 @SpringBootApplication
 public class SafetyNetApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SafetyNetApplication.class, args);
+		
+		
+				
+				
+		
+
 	}
 
 }
